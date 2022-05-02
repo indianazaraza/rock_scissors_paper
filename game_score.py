14969397ddb1adf7dc5f0d23ba2cbdcd5aee6f39
Macarena Acosta <macarenaa988@gmail.com>
2022-05-02 19:28:54 -0300

    change function name line 6

--- game_score.py
+++ game_score.py
@@ -1,11 +1,11 @@
-from de import there_is_a_winning_option
+from de import is_a_winning_option
 
 def score(player_option:int, computer_option:int) -> list:
 	points:list = [0,0]
 
-	if there_is_a_winning_option(player_option, computer_option):
+	if is_a_winning_option(player_option, computer_option):
 		points[0] = 1
-	elif(there_is_a_winning_option(computer_option, player_option)):
+	elif(is_a_winning_option(computer_option, player_option)):
 		points[1] = 1
 	else: 
 		points = [0,0]
