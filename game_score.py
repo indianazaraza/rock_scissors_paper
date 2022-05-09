0bc461ca6105b96d3cbd67a38bd9bf34f0dac649
Macarena Acosta <macarenaa988@gmail.com>
2022-05-09 17:01:09 -0300

    delete else

--- game_score.py
+++ game_score.py
@@ -7,6 +7,4 @@ def score(player_option:int, computer_option:int) -> list:
 		points[0] = 1
 	elif(is_a_winning_option(computer_option, player_option)):
 		points[1] = 1
-	else: 
-		points = [0,0]
 	return points
